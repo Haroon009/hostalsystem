2ec99a113b247afc6f602473e6e60c37ff84a1c8
haroon <haroonahmed09@hotmail.com>
2019-02-09 21:06:48 +0500

    Initial commit

---
+++ resources/views/Insertpage.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+<form method="post">
+
+   Name :<br>
+    <input type="text" name="name"><br>
+    Cnic:<br>
+    <input type="text" name=""><br>
+    AGE:<br>
+    <input type="text" name=""><br>
+    city:<br>
+    <input type="text" name=""><br>
+    state:<br>
+    <input type="text" name=""><br>
+    Cnic:<br>
+    <input type="text" name=""><br>
+    Room id:<br>
+    <input type="text" name="">
+    <br>
+    <input type="button" name="Submit"><br>
+</form>
+
+
+
+</body>
+</html>
